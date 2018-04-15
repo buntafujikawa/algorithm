18550c004d0102a31f40d33ec8befb72725c1a33
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-04-16 08:04:47 +0900

    P195 add BadNeighbors.php

---
+++ cheetah-book/chapter6/BadNeighbors.php
@@ -0,0 +1,48 @@
+<?php
+
+//$donations = [10, 3, 2, 5, 7, 8];
+//$donations = [11, 15];
+$donations = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
+
+$badNeighbors = new BadNeighbors();
+var_dump($badNeighbors->run($donations));
+
+class BadNeighbors
+{
+    function run(array $donations): int
+    {
+        $ans = 0;
+        $count = count($donations);
+        $dp = [];
+
+        for ($i = 0; $i < $count - 1; $i++) {
+            $dp[$i] = $donations[$i];
+
+            if ($i > 0) {
+                $dp[$i] = max($dp[$i], $dp[$i - 1]);
+            }
+
+            if ($i > 1) {
+                $dp[$i] = max($dp[$i], $dp[$i - 2] + $donations[$i]);
+            }
+
+            $ans = max($ans, $dp[$i]);
+        }
+
+        for ($i = 0; $i < $count - 1; $i++) {
+            $dp[$i] = $donations[$i + 1];
+
+            if ($i > 0) {
+                $dp[$i] = max($dp[$i], $dp[$i - 1]);
+            }
+
+            if ($i > 1) {
+                $dp[$i] = max($dp[$i], $dp[$i - 2] + $donations[$i + 1]);
+            }
+
+            $ans = max($ans, $dp[$i]);
+        }
+
+        return $ans;
+    }
+}
