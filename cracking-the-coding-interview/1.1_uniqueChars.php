147773b907600e0fb9bba747bc31e7827aecdea5
buntafujikawa <bunta.fujikawa@gmail.com>
2018-09-30 18:23:02 +0900

    add 1.1 example

--- cracking-the-coding-interview/1.1_uniqueChars.php
+++ cracking-the-coding-interview/1.1_uniqueChars.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * 与えられた文字列に重複がないか判定を行う
+ * 与えられた文字列が全て固有であるかどうかの判定を行う
  */
 function isUniqueChars(string $str): bool
 {
@@ -18,3 +18,28 @@ function isUniqueChars(string $str): bool
 
 var_dump(isUniqueChars("abc") === true);
 var_dump(isUniqueChars("aaa") === false);
+
+/*
+ * example
+ */
+function isUniqueCharsExample(string $str): bool
+{
+    // 文字コードがASCIIの場合
+    if (strlen($str) > 128) {
+        return false;
+    }
+
+    $charSet = [];
+    for ($i = 0; $i < strlen($str); $i++) {
+        $val = substr($str, $i, 1);
+        if (isset($charSet[$val])) {
+            return false;
+        }
+
+        $charSet[$val] = true;
+    }
+    return true;
+}
+
+var_dump(isUniqueCharsExample("abc") === true);
+var_dump(isUniqueCharsExample("aaa") === false);
