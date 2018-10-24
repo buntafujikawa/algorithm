e06e8e63e125ff18dad634d00ffa44ddce565522
buntafujikawa <bunta.fujikawa@gmail.com>
2018-10-24 23:36:02 +0800

    add 2.2 searchNthFromBack

--- cracking-the-coding-interview/2_list/2.2_searchNthFromBack.php
+++ cracking-the-coding-interview/2_list/2.2_searchNthFromBack.php
@@ -11,3 +11,23 @@ function searchNthFromBackUsingCount(array $list, int $k): string
 // test
 var_dump(searchNthFromBackUsingCount(["a", "b", "c"], 1) === "c");
 var_dump(searchNthFromBackUsingCount(["a", "b", "c"], 3) === "a");
+
+/*
+ * リストの長さが分からない場合
+ */
+function searchNthFromBack(array $list, int $k): string
+{
+    $pointer1 = 0;
+    $pointer2 = $k;
+
+    while (isset($list[$pointer2])) {
+        $pointer1++;
+        $pointer2++;
+    }
+
+    return $list[$pointer1];
+}
+
+// test
+var_dump(searchNthFromBack(["a", "b", "c"], 1) === "c");
+var_dump(searchNthFromBack(["a", "b", "c"], 3) === "a");
